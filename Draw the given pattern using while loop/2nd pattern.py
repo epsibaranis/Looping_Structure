9dcc729dd613f5502e41367a8415276e60a96b58
epsibaranis <104891770+epsibaranis@users.noreply.github.com>
2023-03-30 03:49:55 +0530

    First commit

---
+++ Draw the given pattern using while loop/2nd pattern.py
@@ -0,0 +1,16 @@
+# print the factors 
+"""
+******
+******
+******
+******
+******
+******  """
+d=0
+while d<=5:
+    c=0
+    while c<=5:
+        print('*',end='')
+        c=c+1
+    d=d+1
+    print()
