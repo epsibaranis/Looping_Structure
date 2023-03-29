de6cae5b4fe9b6b28039c1446d144c845771bb98
epsibaranis <104891770+epsibaranis@users.noreply.github.com>
2023-03-30 03:59:04 +0530

    First commit

---
+++ Draw the given pattern using while loop/12th pattern.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+"""
+**********
+*        *
+*        *
+*        *
+*        *
+*        *
+**********
+"""
+s=0
+n=10
+while s<n:
+    print('*',end='')
+    s=s+1
+print()    
+c=0
+m=8
+while c<=4:
+    print('*',end='')
+    q=0
+    while q<m:
+        print(' ',end="")
+        q=q+1
+    print('*')
+    c=c+1
+u=0
+v=10
+while u<v:
+         print('*',end='')
+         u=u+1
