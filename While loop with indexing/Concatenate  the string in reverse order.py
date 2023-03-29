69c76b4f98ccf93609ee744c3868e34a09189e5a
epsibaranis <104891770+epsibaranis@users.noreply.github.com>
2023-03-30 04:07:41 +0530

    First commit

---
+++ While loop with indexing/Concatenate  the string in reverse order.py
@@ -0,0 +1,10 @@
+# concatenate  the string in reverse order
+a=input('a=?')
+b=''
+n=len(a)
+i=1
+print("concatenate  the string in reverse order")
+while i<=n:
+         b=b+a[-i]
+         i=i+1
+print(b)    
