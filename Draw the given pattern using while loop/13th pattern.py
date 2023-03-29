8bc19dec22ea25236bfa60d13a07980fbdcccbde
epsibaranis <104891770+epsibaranis@users.noreply.github.com>
2023-03-30 03:59:55 +0530

    First commit

---
+++ Draw the given pattern using while loop/13th pattern.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+"""
+       *
+     *   *
+    *     *
+   *       *
+  *         *
+ *           *
+* * * * * * * * 
+"""
+q=0
+r=7
+while q<r:
+    print(' ',end='')
+    q=q+1
+print('*',end='')
+print()    
+a=0
+b=5
+d=3
+while a<=4:
+    c=0
+    while c<b:
+        print(' ',end='')
+        c=c+1
+    print('*',end='')
+    e=0
+    while e<d:
+        print(' ',end='')
+        e=e+1 
+    print('*',end='')
+    print()
+    b=b-1
+    d=d+2
+    a=a+1 
+f=0
+g=8
+while f<g:
+    print('*',end='')
+    print(' ',end='')
+    f=f+1    
