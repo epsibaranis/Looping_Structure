f05b81e5958da48db431186f31649cdffcedc065
epsibaranis <104891770+epsibaranis@users.noreply.github.com>
2023-03-30 03:23:50 +0530

    First commit

---
+++ While_loop/priint the numbers between two limits say it even or not.py
@@ -0,0 +1,10 @@
+# priint the numbers between two limits say it even or not
+23/12/2021
+c=1
+while c<=100:
+    print(c)
+    if c%2==0:
+        print('The number is even')
+    else:
+        print('The number is  odd')
+        c=c+1 
