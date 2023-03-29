ec136e5cc4efdbc62e2e233298158c38d1329ac9
epsibaranis <104891770+epsibaranis@users.noreply.github.com>
2023-03-30 04:13:54 +0530

    First commit

---
+++ While loop with indexing/copy the first n character from mth position.py
@@ -0,0 +1,12 @@
+#copy the first n character from mth position
+s1=str(input('s1=?'))
+m=int(input('m=?'))
+n=int(input('n=?'))
+s2=''
+i=0
+print("copy the first n character from mth position")
+while i<n:
+    s2=s2+s1[m]
+    i=i+1
+    m=m+1
+print(s2)
