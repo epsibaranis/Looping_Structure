054d8ae6a2f1f0db9f8a509f302c1c1f3814e8ff
epsibaranis <104891770+epsibaranis@users.noreply.github.com>
2023-03-30 06:07:18 +0530

    fc

---
+++ While loop in data structure/List/List of n students Bio-data.py
@@ -0,0 +1,19 @@
+# List of n students Bio-data
+n=int(input('n=?'))
+b=[]
+i=0
+while i<n:
+       a=[]
+       x=input('Name   : ')
+       y=input('Age    : ')
+       c=input('Gender : ')
+       d=input('Address: ')
+       e=input('Emailid: ')
+       a.append(x)
+       a.append(y)
+       a.append(c)
+       a.append(d)
+       a.append(e)
+       b.append(a)
+       i+=1
+print('List of students Bio-data:',b)
