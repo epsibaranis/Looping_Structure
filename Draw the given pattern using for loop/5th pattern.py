0dba724f30c60a4e3197d520c9dbcc8a604e643d
epsibaranis <104891770+epsibaranis@users.noreply.github.com>
2023-03-30 06:42:05 +0530

    fc

---
+++ Draw the given pattern using for loop/5th pattern.py
@@ -0,0 +1,18 @@
+"""
+*
+**
+***
+****
+*****
+******
+*****
+****
+***
+**
+* pirnt the pattern using for loop
+"""
+print("print the pattern using for loop")
+for i in range (7):
+    print('*'*i)
+for i in range (5,0,-1):
+    print('*'*i)
