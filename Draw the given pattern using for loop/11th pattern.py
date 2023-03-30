0a5ad89cf975348e346b176fc42aea056c273815
epsibaranis <104891770+epsibaranis@users.noreply.github.com>
2023-03-30 06:54:05 +0530

    fc

---
+++ Draw the given pattern using for loop/11th pattern.py
@@ -0,0 +1,15 @@
+"""
+pgm no 157
+*********
+*       *
+*       *
+*       *
+*       *
+*       *
+********* print the pattern using for loop
+"""
+print("print the pattern using for loop")
+print('*'*9)
+for i in range(5):
+    print('*'+' '*7+'*')
+print('*'*9)
