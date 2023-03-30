da6ebb68ee8c5f44479319b0d8fabd621cfc4404
epsibaranis <104891770+epsibaranis@users.noreply.github.com>
2023-03-30 06:51:23 +0530

    fc

---
+++ Draw the given pattern using for loop/8th pattern.py
@@ -0,0 +1,13 @@
+"""
+     *
+    ***
+   *****
+  *******
+ *********
+*********** print the pattern using for loop
+""" 
+print("print the pattern using for loop")
+j=1
+for i in range(6,0,-1):
+    print(' '*i+'*'*j)
+    j+=2
