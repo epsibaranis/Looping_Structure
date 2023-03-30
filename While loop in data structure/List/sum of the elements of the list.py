d219ecc3fbc3758024580324caec2ba22c69ce1b
epsibaranis <104891770+epsibaranis@users.noreply.github.com>
2023-03-30 05:53:13 +0530

    fc

---
+++ While loop in data structure/List/sum of the elements of the list.py
@@ -0,0 +1,16 @@
+# sum of the elements of the list
+a=[]
+n=int(input('n=?'))
+i=0
+while i<n:
+    x=int(input('x=?'))
+    a.append(x)
+    i=i+1
+print("list",a)
+m=0
+s=0
+while m<n:
+    a[m]
+    s=s+a[m]
+    m=m+1
+print("sumof the element in the list:",s)
