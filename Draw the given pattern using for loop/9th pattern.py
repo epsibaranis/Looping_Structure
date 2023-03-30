b001fa90331b5f87983c64806f02fc270028b28c
epsibaranis <104891770+epsibaranis@users.noreply.github.com>
2023-03-30 06:52:27 +0530

    fc

---
+++ Draw the given pattern using for loop/9th pattern.py
@@ -0,0 +1,13 @@
+"""
+***********
+ *********
+  *******
+   *****
+    ***
+     *  print the pattern using for loop
+"""
+print("print the pattern using for loop")
+j=11
+for i in range(6):
+    print(' '*i+'*'*j)
+    j-=2
